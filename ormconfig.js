e5ddfa550e0cef2fa1394f78c40de25b2834c7a0
Егор Попов <eapopov@asuproject.ru>
2021-04-30 19:46:35 +0500

    fix ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -7,11 +7,7 @@ if (isDev) {
 } else {
     dbConfig = {
         type: process.env.DB_TYPE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB
+        url: process.env.DB_URL
     }
 }
 
